86975393e82b5215a633e8a46532773d913b157c
Madani Bezoui <madani.bezoui@gmail.com>
2026-09-06 20:19:56 +0200

    Clean up temporary scripts

--- fix_tab4.py
+++
@@ -1,7 +0,0 @@
-import re
-with open("Soumission/main_paper.tex", "r") as f:
-    text = f.read()
-
-# wait, just checking one final thing about Table 4: did the 0.11% come back because of missing 100 multiplier?
-# The table 4 is "Annualized Mean Return (\%) & 12.24 & 10.75 & 10.76 & 11.15 & 11.28 \\"
-# So it's 12.24, not 0.12. It's completely fine.
